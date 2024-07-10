b1ef21e944ad59b0be975b65196e111e1c96cf55
helozen <shaktishihab247#gmail.com>
2024-07-10 19:54:33 +0600

    Editing urls

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -16,3 +16,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
+
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import UserCreate, JobListCreate, BookingListCreate, ReviewListCreate
+
+urlpatterns = [
+    path('register/', UserCreate.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jobs/', JobListCreate.as_view(), name='job-list-create'),
+    path('bookings/', BookingListCreate.as_view(), name='booking-list-create'),
+    path('reviews/', ReviewListCreate.as_view(), name='review-list-create'),
+]
